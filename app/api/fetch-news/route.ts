cc3f221e74ac1ea4f384dc12c69d4193d5fed45c
soramameen <sorananonaomasa.sora@gmail.com>
2025-03-16 10:24:28 +0900

    newsAPIのfetch成功

---
+++ app/api/fetch-news/route.ts
@@ -0,0 +1,31 @@
+import { Article, QuizData } from "@/app/types";
+import { writeFile } from "fs/promises";
+import { NextResponse } from "next/server";
+import { join } from "path";
+export async function GET(): Promise<NextResponse> {
+  try {
+    const url = `https://newsapi.org/v2/top-headlines?category=technology&pageSize=5&apiKey=${process.env.NEWS_API_KEY}`;
+    const res = await fetch(url);
+    if (!res.ok) {
+      throw new Error("Failed to fetch news");
+    }
+    const data = (await res.json()) as { articles: Article[] };
+    if (data.articles.length < 5) {
+      throw new Error("ニュースが不足しています");
+    }
+
+    const articles: Article[] = data.articles.slice(0, 5).map((article) => ({
+      title: article.title,
+    }));
+    const quizData: QuizData = { articles };
+    const filePath = join(process.cwd(), "app/data/quiz.json");
+    await writeFile(filePath, JSON.stringify(quizData, null, 2));
+
+    return NextResponse.json({ message: "ニュース取得完了", articles });
+  } catch (error) {
+    return NextResponse.json(
+      { error: (error as Error).message },
+      { status: 500 }
+    );
+  }
+}
---
+++ app/types.ts
@@ -0,0 +1,6 @@
+export interface Article {
+  title: string;
+}
+export interface QuizData {
+  articles: Article[];
+}
